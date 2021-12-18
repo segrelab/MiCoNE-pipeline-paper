47940959f79d7656f19d22138f4442ace0ec34f7
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-18 09:13:19 -0500

    Fix: Figure 2 pie chart now uses ggrepel

--- scripts/figure2/figure2_plot.R
+++ scripts/figure2/figure2_plot.R
@@ -5,13 +5,11 @@ library(ggplot2)
 library(ggpubr)
 library(ggrepel)
 
-plot_scatter <- function(data) {
+plot_scatter <- function(data, color_var) {
   ggscatter(data,
     x = "X0", y = "X1",
-    color = "NI", shape = "TA",
-   #ylim=c(-1, 1),
-   #xlim=c(-1, 1),
-   #ellipse = TRUE
+    color = color_var,
+    #ellipse = TRUE
   )
 }
 
@@ -19,23 +17,30 @@ x <- read.csv("../../data/figure2/output/moving_pictures/x.csv")
 y_reduced <- read.csv("../../data/figure2/output/moving_pictures/y_reduced2.csv")
 names(y_reduced)[names(y_reduced)=="X"] <- "hash"
 percentage_variance <- read.csv("../../data/figure2/output/moving_pictures/percentage_variance.csv")
-percentage_variance$labels <- paste0(percentage_variance$X, " (", formatC(percentage_variance$sum_sq, digits = 3, format = "f"), "%)")
+
+percentage_variance <-
+  percentage_variance %>%
+  mutate(labels = scales::percent(sum_sq, scale=1)) %>%
+  arrange(desc(X)) %>%
+  mutate(text_y = cumsum(sum_sq) - sum_sq / 2)
 
 
 # df <- y_reduced %>% select("hash", "X0", "X1") %>% filter((abs(X0) < 5) & (abs(X1) < 10)) %>% left_join(x, by="hash")
 df <- y_reduced %>% select("hash", "X0", "X1") %>% left_join(x, by="hash")
-scat <- plot_scatter(df)
+scatter_ta <- plot_scatter(df, "TA")
+scatter_ni <- plot_scatter(df, "NI")
 
-# Maybe repel: https://stackoverflow.com/questions/69715282/how-to-adjust-ggrepel-label-on-pie-chart
-pie_chart <- ggpie(
-                   percentage_variance,
-                   "sum_sq",
-                   label="labels",
-                   fill="X",
-                   palette="Set2",
-                   lab.pos="out",lab.font="black",
-                   repel = TRUE,
-)
+pie_chart <- ggplot(data = percentage_variance, aes(x = "", y = sum_sq, fill = X)) +
+                       geom_bar(stat = "identity") + 
+                       geom_label_repel(aes(label = labels, y = text_y), nudge_x = 1.6) +
+                       coord_polar(theta = "y") +
+                       theme_pubr() +
+                       theme(axis.ticks = element_blank(),
+                             axis.line = element_blank(),
+                             axis.title = element_blank(),
+                            axis.text.y = element_blank(),
+                            axis.text.x = element_blank())
 
-ggarrange(pie_chart, scat, labels = c("A", "B"))
+scatter_facet = ggarrange(scatter_ta, scatter_ni)
+ggarrange(pie_chart, scatter_facet, labels = c("A", "B"), ncol = 1)
 ggsave("figure2.pdf", width = 14, height = 12)
