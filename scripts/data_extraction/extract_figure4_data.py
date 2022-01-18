f1e15534100afb8cccbc0796044949b5e92baae5
Dileep Kishore <k.dileep1994@gmail.com>
2022-01-18 12:56:12 -0500

    Add: Data extraction script for Figure 4

---
+++ scripts/data_extraction/extract_figure4_data.py
@@ -0,0 +1,29 @@
+import pathlib
+
+from data_extraction import extract_data
+
+TA_METHODS = ("blast(ncbi)", "naive_bayes(gg_13_8_99)", "naive_bayes(silva_138_99)")
+
+
+def extract_figure4_data(
+    input_folder: pathlib.Path, output_folder: pathlib.Path
+) -> None:
+    # STEP1: Loop through each TA method
+    for ta_method in TA_METHODS:
+        workflow = "tax_assignment"
+        module = ta_method
+        process = "taxonomy_tables"
+        prev_process = "dada2-remove_bimera"
+        # STEP2: Extract the data
+        output_subfolder = output_folder / module
+        extract_data(
+            input_folder, workflow, module, process, prev_process, output_subfolder
+        )
+
+
+if __name__ == "__main__":
+    INPUT_FOLDER = pathlib.Path(
+        "/home/dileep/Documents/Work/MIND/Results/micone_scc_testing/full_pipeline_testing/outputs/outputs"
+    )
+    OUTPUT_FOLDER = pathlib.Path("../../data/figure4/input/moving_pictures")
+    extract_figure4_data(INPUT_FOLDER, OUTPUT_FOLDER)
