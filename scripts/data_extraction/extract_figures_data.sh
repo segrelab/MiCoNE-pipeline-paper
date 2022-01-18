bdd2d8dce86e833bc6e9e98d8be936432c7151bd
Dileep Kishore <k.dileep1994@gmail.com>
2022-01-18 16:32:01 -0500

    Add: Qsub options to data extraction script

--- scripts/data_extraction/extract_figures_data.sh
+++ scripts/data_extraction/extract_figures_data.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+#$ -l h_rt=4:00:00
+#$ -N micone_data_extraction
+#$ -P visant
+#$ -o qsub_outputs.txt
+#$ -e qsub_errors.txt
+#$ -m e
+#$ -pe omp 4
+
 # TODO: Change this before running
 INPUT_FOLDER="/home/dileep/Documents/Work/MIND/Results/micone_scc_testing/full_pipeline_testing/outputs/outputs"
 DATASET="moving_pictures"
