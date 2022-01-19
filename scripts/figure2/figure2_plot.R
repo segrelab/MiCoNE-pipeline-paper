40c1d15db9319306553f8b08306983b53e421985
Dileep Kishore <k.dileep1994@gmail.com>
2022-01-19 11:25:58 -0500

    Change: Add cli args to Figure 2 plotting script

--- scripts/figure2/figure2_plot.R
+++ scripts/figure2/figure2_plot.R
@@ -5,42 +5,61 @@ library(ggplot2)
 library(ggpubr)
 library(ggrepel)
 
+# inputs
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+    data_folder <- "../../data/figure2/output/moving_pictures/"
+    output_folder <- "../../figures/"
+} else if (length(args) == 2) {
+    data_folder <- args[1]
+    output_folder <- args[2]
+} else {
+    stop("Required number of arguments must equal 2")
+}
+x_csv <- paste0(data_folder, "x.csv")
+y_reduced_csv <- paste0(data_folder, "y_reduced2.csv")
+percentage_variance_csv <- paste0(data_folder, "percentage_variance.csv")
+output_file <- paste0(output_folder, "figure2.pdf")
+
 plot_scatter <- function(data, color_var) {
-  ggscatter(data,
-    x = "X0", y = "X1",
-    color = color_var,
-    #ellipse = TRUE
-  )
+    ggscatter(data,
+        x = "X0", y = "X1",
+        color = color_var,
+        # ellipse = TRUE
+    )
 }
 
-x <- read.csv("../../data/figure2/output/moving_pictures/x.csv")
-y_reduced <- read.csv("../../data/figure2/output/moving_pictures/y_reduced2.csv")
-names(y_reduced)[names(y_reduced)=="X"] <- "hash"
-percentage_variance <- read.csv("../../data/figure2/output/moving_pictures/percentage_variance.csv")
+x <- read.csv(x_csv)
+y_reduced <- read.csv(y_reduced_csv)
+names(y_reduced)[names(y_reduced) == "X"] <- "hash"
+percentage_variance <- read.csv(percentage_variance_csv)
 
 percentage_variance <-
-  percentage_variance %>%
-  mutate(labels = scales::percent(mean_sq, scale=1)) %>%
-  arrange(desc(X)) %>%
-  mutate(text_y = cumsum(mean_sq) - mean_sq / 2)
+    percentage_variance %>%
+    mutate(labels = scales::percent(mean_sq, scale = 1)) %>%
+    arrange(desc(X)) %>%
+    mutate(text_y = cumsum(mean_sq) - mean_sq / 2)
 
 
-# df <- y_reduced %>% select("hash", "X0", "X1") %>% filter((abs(X0) < 5) & (abs(X1) < 10)) %>% left_join(x, by="hash")
-df <- y_reduced %>% select("hash", "X0", "X1") %>% left_join(x, by="hash")
+df <- y_reduced %>%
+    select("hash", "X0", "X1") %>%
+    left_join(x, by = "hash")
 scatter_ta <- plot_scatter(df, "TA")
 scatter_ni <- plot_scatter(df, "NI")
 
 pie_chart <- ggplot(data = percentage_variance, aes(x = "", y = mean_sq, fill = X)) +
-                       geom_bar(stat = "identity") + 
-                       geom_label_repel(aes(label = labels, y = text_y), nudge_x = 1.6) +
-                       coord_polar(theta = "y") +
-                       theme_pubr() +
-                       theme(axis.ticks = element_blank(),
-                             axis.line = element_blank(),
-                             axis.title = element_blank(),
-                            axis.text.y = element_blank(),
-                            axis.text.x = element_blank())
+    geom_bar(stat = "identity") +
+    geom_label_repel(aes(label = labels, y = text_y), nudge_x = 1.6) +
+    coord_polar(theta = "y") +
+    theme_pubr() +
+    theme(
+        axis.ticks = element_blank(),
+        axis.line = element_blank(),
+        axis.title = element_blank(),
+        axis.text.y = element_blank(),
+        axis.text.x = element_blank()
+    )
 
-scatter_facet = ggarrange(scatter_ta, scatter_ni)
+scatter_facet <- ggarrange(scatter_ta, scatter_ni, ncol = 2)
 ggarrange(pie_chart, scatter_facet, labels = c("A", "B"), ncol = 1)
-ggsave("figure2.pdf", width = 14, height = 12)
+ggsave(output_file, width = 14, height = 12)
