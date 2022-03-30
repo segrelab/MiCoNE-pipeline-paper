e4afbe0ba482aa91b51edac28910255af2388fd0
Dileep Kishore <k.dileep1994@gmail.com>
2022-03-30 11:24:43 +0530

    Fix: Make data transfer use single rsync call

--- data_transfer.sh
+++ data_transfer.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 if [ $1 == "scc" ]; then
-  rsync -r --progress ~/Documents/Work/MIND/Manuscripts/MiCoNE-pipeline-paper/data scc4:/rprojectnb/visant/dkishore/MiCoNE-pipeline-paper/
-  rsync -r --progress ~/Documents/Work/MIND/Manuscripts/MiCoNE-pipeline-paper/figures scc4:/rprojectnb/visant/dkishore/MiCoNE-pipeline-paper/
+  rsync -r --progress ~/Documents/Work/MIND/Manuscripts/MiCoNE-pipeline-paper/{data,figures} scc4:/rprojectnb/visant/dkishore/MiCoNE-pipeline-paper/
 fi
 
 
 if [ $1 == "local" ]; then
-  rsync -r --progress scc4:/rprojectnb/visant/dkishore/MiCoNE-pipeline-paper/data ~/Documents/Work/MIND/Manuscripts/MiCoNE-pipeline-paper/
-  rsync -r --progress scc4:/rprojectnb/visant/dkishore/MiCoNE-pipeline-paper/figures ~/Documents/Work/MIND/Manuscripts/MiCoNE-pipeline-paper/
+  rsync -r --progress scc4:/rprojectnb/visant/dkishore/MiCoNE-pipeline-paper/{data,figures} ~/Documents/Work/MIND/Manuscripts/MiCoNE-pipeline-paper/
 fi
