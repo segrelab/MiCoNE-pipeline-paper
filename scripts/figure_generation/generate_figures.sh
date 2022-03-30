9903fbb97b410663e80c0b86bd33113549e9a7ee
Dileep Kishore <k.dileep1994@gmail.com>
2022-03-30 21:08:33 +0530

    Fix: Paths in figure generation script

--- scripts/figure_generation/generate_figures.sh
+++ scripts/figure_generation/generate_figures.sh
@@ -11,7 +11,7 @@
 # TODO: Change this before running
 PLATFORM="scc"
 DATASET="moving_pictures_scc"
-OUTPUT_DIR="../../figures/moving_pictures_scc"
+OUTPUT_DIR="../../figures/moving_pictures_scc/"
 mkdir $OUTPUT_DIR
 
 if [ $PLATFORM == "scc" ]; then
@@ -22,13 +22,13 @@ fi
 # Figure 2
 echo "Generating Figure 2"
 cd ../figure2/
-Rscript figure2_plot.R "../../data/figure2/output/$DATASET" "$OUTPUT_DIR"
+Rscript figure2_plot.R "../../data/figure2/output/$DATASET/" "$OUTPUT_DIR"
 
 # Figure 3
 echo "Generating Figure 3"
 cd ../figure3/
 Rscript figure3_plot.R \
-  "../../data/figure3/output/$DATASET" \
+  "../../data/figure3/output/$DATASET/" \
   "../../data/figure3/output/" \
   "$OUTPUT_DIR"
 
@@ -36,21 +36,21 @@ Rscript figure3_plot.R \
 echo "Generating Figure 4"
 cd ../figure4/
 Rscript figure4_plot.R \
-  "../../data/figure4/output/$DATASET" \
+  "../../data/figure4/output/$DATASET/" \
   "../../data/figure4/output/" \
   "$OUTPUT_DIR"
 
 # Figure 5
 echo "Generating Figure 5"
 cd ../figure5/
-Rscript figure5_plot.R "../../data/figure5/output/$DATASET" "$OUTPUT_DIR"
+Rscript figure5_plot.R "../../data/figure5/output/$DATASET/" "$OUTPUT_DIR"
 
 # Figure 6
 echo "Generating Figure 6"
 cd ../figure6/
-Rscript figure6_plot.R "../../data/figure6/output/$DATASET" "$OUTPUT_DIR"
+Rscript figure6_plot.R "../../data/figure6/output/$DATASET/" "$OUTPUT_DIR"
 
 # Figure 7
 echo "Generating Figure 7"
 cd ../figure7/
-Rscript figure7_plot.R "../../data/figure7/output/norta" "$OUTPUT_DIR"
+Rscript figure7_plot.R "../../data/figure7/output/norta/" "$OUTPUT_DIR"
