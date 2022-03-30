d25447e0180aa56d8f9b644b54c075a391e28c4f
Dileep Kishore <k.dileep1994@gmail.com>
2022-03-30 11:27:05 +0530

    Change: mkdir in processing script + change figure output location

--- scripts/figure_generation/generate_figures.sh
+++ scripts/figure_generation/generate_figures.sh
@@ -11,6 +11,7 @@
 # TODO: Change this before running
 PLATFORM="local"
 DATASET="moving_pictures"
+OUTPUT_DIR="../../figures/moving_pictures"
 
 if [ $PLATFORM == "scc" ]; then
   module load miniconda
@@ -20,7 +21,7 @@ fi
 # Figure 2
 echo "Generating Figure 2"
 cd ../figure2/
-Rscript figure2_plot.R "../../data/figure2/output/$DATASET" "../../figures"
+Rscript figure2_plot.R "../../data/figure2/output/$DATASET" "$OUTPUT_DIR"
 
 # Figure 3
 echo "Generating Figure 3"
@@ -28,7 +29,7 @@ cd ../figure3/
 Rscript figure3_plot.R \
   "../../data/figure3/output/$DATASET" \
   "../../data/figure3/output/" \
-  "../../figures"
+  "$OUTPUT_DIR"
 
 # Figure 4
 echo "Generating Figure 4"
@@ -36,19 +37,19 @@ cd ../figure4/
 Rscript figure4_plot.R \
   "../../data/figure4/output/$DATASET" \
   "../../data/figure4/output/" \
-  "../../figures"
+  "$OUTPUT_DIR"
 
 # Figure 5
 echo "Generating Figure 5"
 cd ../figure5/
-Rscript figure5_plot.R "../../data/figure5/output/$DATASET" "../../figures"
+Rscript figure5_plot.R "../../data/figure5/output/$DATASET" "$OUTPUT_DIR"
 
 # Figure 6
 echo "Generating Figure 6"
 cd ../figure6/
-Rscript figure6_plot.R "../../data/figure6/output/$DATASET" "../../figures"
+Rscript figure6_plot.R "../../data/figure6/output/$DATASET" "$OUTPUT_DIR"
 
 # Figure 7
 echo "Generating Figure 7"
 cd ../figure7/
-Rscript figure7_plot.R "../../data/figure7/output/norta" "../../figures"
+Rscript figure7_plot.R "../../data/figure7/output/norta" "$OUTPUT_DIR"
