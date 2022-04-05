7888f7f09325bfa2171e2065c197600ed4a1079d
Dileep Kishore <k.dileep1994@gmail.com>
2022-04-05 04:00:15 -0400

    Fix: cd commands in batch figure script

--- scripts/run_figure_script.sh
+++ scripts/run_figure_script.sh
@@ -3,11 +3,14 @@
 cd data_extraction
 rm *.txt
 qsub extract_figures_data.sh
+cd ../
 
 cd data_processing
 rm *.txt
 qsub process_figures_data.sh
+cd ../
 
 cd figure_generation
 rm *.txt
 qsub generate_figures.sh
+cd ../
