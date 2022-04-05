4c7ca91e69258cca3cec8a792e16b911e48a289a
Dileep Kishore <k.dileep1994@gmail.com>
2022-04-05 16:15:58 +0530

    Fix: Figure 3 and 4 data extraction meta_id_list added

--- scripts/data_extraction/extract_figure3_data.py
+++ scripts/data_extraction/extract_figure3_data.py
@@ -13,7 +13,7 @@ DC_METHODS = (
 
 
 def extract_figure3_data(
-    input_folder: pathlib.Path, output_folder: pathlib.Path
+    input_folder: pathlib.Path, output_folder: pathlib.Path, meta_id_list: list
 ) -> None:
     # STEP1: Loop through each DC method
     for dc_method in DC_METHODS:
@@ -22,11 +22,14 @@ def extract_figure3_data(
         process = "hashed_output"
         # STEP2: Extract the data
         output_subfolder = output_folder / module
-        extract_data(input_folder, workflow, module, process, "", output_subfolder)
+        extract_data(
+            input_folder, workflow, module, process, "", output_subfolder, meta_id_list
+        )
 
 
 if __name__ == "__main__":
     INPUT_FOLDER = pathlib.Path(sys.argv[1])
     DATASET = sys.argv[2]
+    META_ID_LIST = sys.argv[3:]
     OUTPUT_FOLDER = pathlib.Path(f"../../data/figure3/input/{DATASET}")
-    extract_figure3_data(INPUT_FOLDER, OUTPUT_FOLDER)
+    extract_figure3_data(INPUT_FOLDER, OUTPUT_FOLDER, META_ID_LIST)
--- scripts/data_extraction/extract_figure4_data.py
+++ scripts/data_extraction/extract_figure4_data.py
@@ -7,7 +7,7 @@ TA_METHODS = ("blast(ncbi)", "naive_bayes(gg_13_8_99)", "naive_bayes(silva_138_9
 
 
 def extract_figure4_data(
-    input_folder: pathlib.Path, output_folder: pathlib.Path
+    input_folder: pathlib.Path, output_folder: pathlib.Path, meta_id_list: list
 ) -> None:
     # STEP1: Loop through each TA method
     for ta_method in TA_METHODS:
@@ -18,12 +18,19 @@ def extract_figure4_data(
         # STEP2: Extract the data
         output_subfolder = output_folder / module
         extract_data(
-            input_folder, workflow, module, process, prev_process, output_subfolder
+            input_folder,
+            workflow,
+            module,
+            process,
+            prev_process,
+            output_subfolder,
+            meta_id_list,
         )
 
 
 if __name__ == "__main__":
     INPUT_FOLDER = pathlib.Path(sys.argv[1])
     DATASET = sys.argv[2]
+    META_ID_LIST = sys.argv[3:]
     OUTPUT_FOLDER = pathlib.Path(f"../../data/figure4/input/{DATASET}")
-    extract_figure4_data(INPUT_FOLDER, OUTPUT_FOLDER)
+    extract_figure4_data(INPUT_FOLDER, OUTPUT_FOLDER, META_ID_LIST)
