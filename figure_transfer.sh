50d4ed765793784608eaee05cf8cfa64bc318d2a
Dileep Kishore <k.dileep1994@gmail.com>
2022-04-08 19:09:00 +0530

    Add: Figure transfer script

---
+++ figure_transfer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $1 == "scc" ]; then
+  rsync -r --progress ~/Documents/Work/MIND/Manuscripts/MiCoNE-pipeline-paper/figures scc4:/rprojectnb/visant/dkishore/MiCoNE-pipeline-paper/
+fi
+
+if [ $1 == "local" ]; then
+  rsync -r --progress scc4:/rprojectnb/visant/dkishore/MiCoNE-pipeline-paper/figures ~/Documents/Work/MIND/Manuscripts/MiCoNE-pipeline-paper/
+fi
