0ed11e81c8f7936d2370c8e1025ebafa41d9e344
Dileep Kishore <k.dileep1994@gmail.com>
2022-09-13 16:21:31 -0400

    Change: Update word counts

--- sections/word_counts.py
+++ sections/word_counts.py
@@ -3,6 +3,7 @@ import subprocess
 
 
 def main(files: list[pathlib.Path]) -> None:
+    data = dict()
     for file in files:
         cmd = f"texcount -brief {file}"
         process_output = subprocess.run(
@@ -10,6 +11,15 @@ def main(files: list[pathlib.Path]) -> None:
         ).stdout.strip()
         word_count = eval(process_output.split(" ")[0])
         print(f"{file.name} has {word_count} words")
+        data[file.stem] = word_count
+    final_word_count = (
+        data["abstract"]
+        + data["discussion"]
+        + data["introduction"]
+        + data["results"]
+        + data["acknowledgements"]
+    )
+    print(f"Final word count = {final_word_count} words")
 
 
 if __name__ == "__main__":
