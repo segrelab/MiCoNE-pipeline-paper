94238bdfc9b8ae2e1ab777ca9649c67222c73416
Dileep Kishore <k.dileep1994@gmail.com>
2022-12-01 10:56:17 -0500

    Add: Col and row version of Figure 5 w/ sensitivity

--- scripts/figure5/figure5_plot.R
+++ scripts/figure5/figure5_plot.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(ggpubr)
 library(tidyverse)
+library(reshape2)
 
 # inputs
 args <- commandArgs(trailingOnly = TRUE)
@@ -45,6 +46,54 @@ plot_boxplot <- function(data, title) {
         labs(title = title, fill = "NI method")
 }
 
+plot_boxplot_precision <- function(data, title) {
+    my_comparisons <- list(c("spieceasi", "SS[0.667]"), c("spieceasi", "SV[1.000]"))
+    precision_plot <- ggboxplot(data, x = "title", y = "precision", facet.by = "type", color = "type", add = "jitter") +
+        facet_grid(. ~ type, scales = "free", space = "free") +
+        theme_pubr() +
+        theme(
+            axis.text.x = element_blank(),
+            text = element_text(size = 18),
+        ) +
+        ylim(0, 1) +
+        xlab("") +
+        ylab("Precision") +
+        labs(title = title, fill = "NI method")
+}
+
+plot_boxplot_sensitivity <- function(data, title) {
+    my_comparisons <- list(c("spieceasi", "SS[0.667]"), c("spieceasi", "SV[1.000]"))
+    sensitivity_plot <- ggboxplot(data, x = "title", y = "sensitivity", facet.by = "type", color = "type", add = "jitter") +
+        facet_grid(. ~ type, scales = "free", space = "free") +
+        theme_pubr() +
+        theme(
+            axis.text.x = element_text(angle = 30, hjust = 1),
+            text = element_text(size = 18),
+        ) +
+        ylim(0, 1) +
+        xlab("Algorithm") +
+        ylab("Sensitivity") +
+        labs(fill = "NI method")
+}
+
+plot_boxplot_all <- function(data, title) {
+    data_subset <- data[, c("type", "title", "precision", "sensitivity")]
+    data_melt <- melt(data_subset, id = c("type", "title"))
+    colnames(data_melt)[colnames(data_melt) == "variable"] <- "Metric"
+    my_comparisons <- list(c("spieceasi", "SS[0.667]"), c("spieceasi", "SV[1.000]"))
+    sensitivity_plot <- ggboxplot(data_melt, x = "title", y = "value", facet.by = "type", color = "Metric", add = "jitter") +
+        facet_grid(. ~ type, scales = "free", space = "free") +
+        theme_pubr() +
+        theme(
+            axis.text.x = element_text(angle = 30, hjust = 1),
+            text = element_text(size = 18),
+        ) +
+        ylim(0, 1) +
+        xlab("Algorithm") +
+        ylab("Precision/Sensitivity") +
+        labs(title = title, fill = "NI method")
+}
+
 methods <- c("propr", "sparcc", "flashweave", "spieceasi", "SS[0.333]", "SS[0.667]", "SS[1.000]", "SV[0.333]", "SV[0.667]", "SV[1.000]")
 norta <- read.csv(norta_performance_csv, sep = ",", header = TRUE)
 norta <- subset(norta, title %in% methods)
@@ -59,8 +108,14 @@ seqtime[seqtime["type"] == "IND", "type"] <- "Individual"
 seqtime[seqtime["type"] == "SS", "type"] <- "SS (consensus)"
 seqtime[seqtime["type"] == "SV", "type"] <- "SV (consensus)"
 
-plot_norta <- plot_boxplot(norta, "NorTA")
-plot_seqtime <- plot_boxplot(seqtime, "Seqtime")
+plot_norta_precision <- plot_boxplot_precision(norta, "NorTA")
+plot_norta_sensitivity <- plot_boxplot_sensitivity(norta, "NorTA")
+plot_seqtime_precision <- plot_boxplot_precision(seqtime, "Seqtime")
+plot_seqtime_sensitivity <- plot_boxplot_sensitivity(seqtime, "Seqtime")
 
+plot_norta <- plot_boxplot_all(norta, "NorTA")
+plot_seqtime <- plot_boxplot_all(seqtime, "Seqtime")
+
+# final_plot <- ggarrange(plot_norta_precision, plot_norta_sensitivity, plot_seqtime_precision, plot_seqtime_sensitivity, ncol = 1, labels = c("A", "B"), common.legend = TRUE, legend = "right")
 final_plot <- ggarrange(plot_norta, plot_seqtime, ncol = 1, labels = c("A", "B"), common.legend = TRUE, legend = "right")
 ggsave(output_file, width = 14, height = 12)
